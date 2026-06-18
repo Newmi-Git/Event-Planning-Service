55ba3f691401b4a7738dcfdb171df03d72203b2b
Owam Gobinca <owamgobingca@gmail.com>
2026-06-18 09:53:36 +0200

    updated JavaScript

--- about.js
+++ about.js
@@ -1,9 +1,27 @@
-console.log("JavaScript loaded")
+// console.log("JavaScript loaded")
 
+// const cards = document.querySelectorAll(".team-card");
+
+// cards.forEach(card => {
+//      card.addEventListener("click", () => {
+//       card.classList.toggle("active");
+//      });
+// });
 const cards = document.querySelectorAll(".team-card");
 
 cards.forEach(card => {
-     card.addEventListener("click", () => {
-      card.classList.toggle("active");
-     });
+    card.addEventListener("click", () => {
+
+        // Hide all other descriptions
+        
+
+        // Toggle the clicked card
+        card.classList.toggle("active");
+
+        cards.forEach(c => {
+            if (c !== card) {
+                c.classList.remove("active");
+            }
+        });
+    });
 });
